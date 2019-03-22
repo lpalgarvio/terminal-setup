d4d6af3ad68e9b2da19debe9e6030550329eb88b
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-03-22 23:43:44 +0000

    Updated vagrant packages in contrib-cli scripts

--- ubuntu/software/contrib-cli/development/virtualization/vagrant.sh
+++ ubuntu/software/contrib-cli/development/virtualization/vagrant.sh
@@ -2,7 +2,7 @@
 # https://www.vagrantup.com/downloads.html
 
 # Releases
-app_release='2.1.2';
+app_release='2.2.4';
 
 # Install software
 wget https://releases.hashicorp.com/vagrant/${app_release}/vagrant_${app_release}_x86_64.deb -P /tmp/ -O /tmp/vagrant_${app_release}_x86_64.deb;
