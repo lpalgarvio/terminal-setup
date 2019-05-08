71e3a0e59ee6d343c5843fe3e341719da8eca330
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-05-08 22:54:49 +0100

    Largely improved the organization of the scripts

--- debian/software/contrib-gui/development/editor/atom.sh
+++ debian/software/contrib-gui/development/editor/atom.sh
@@ -4,7 +4,7 @@
 # Repositories and keys
 wget -O- https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -;
 sudo bash -c "cat <<EOF > /etc/apt/sources.list.d/atom.list
-# Atom Editor
+# Atom
 deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main
 EOF";
 sudo apt-get update;
