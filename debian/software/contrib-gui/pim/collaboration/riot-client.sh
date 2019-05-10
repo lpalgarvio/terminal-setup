74b455583e0477e5b5789ec847d414247fa71852
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-05-10 21:36:50 +0100

    Updated Riot script

--- debian/software/contrib-gui/pim/collaboration/riot-client.sh
+++ debian/software/contrib-gui/pim/collaboration/riot-client.sh
@@ -6,10 +6,12 @@ os_distro=`lsb_release -si | tr '[:upper:]' '[:lower:]'`;
 os_codename=`lsb_release -sc | tr '[:upper:]' '[:lower:]'`;
 
 # Repositories and keys
-curl -sL https://riot.im/packages/${os_distro}/repo-key.asc | sudo apt-key add -;
-sudo bash -c "cat <<EOF > /etc/apt/sources.list.d/matrix-riot-im.list
+sudo apt-key remove 6FEB6F83D48B93547E7DFEDEE019645248E8F4A1;
+sudo wget -O /usr/share/keyrings/riot-im-archive-keyring.gpg https://packages.riot.im/debian/riot-im-archive-keyring.gpg;
+sudo rm -f /etc/apt/sources.list.d/matrix-riot-im.list*;
+sudo bash -c "cat <<EOF > /etc/apt/sources.list.d/riot-im.list
 # Riot Client
-deb [arch=amd64] https://riot.im/packages/${os_distro}/ ${os_codename} main
+deb [signed-by=/usr/share/keyrings/riot-im-archive-keyring.gpg] https://packages.riot.im/debian/ ${os_codename} main
 EOF";
 sudo apt-get update;
 
