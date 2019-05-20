eb4c1d6543ae5b666c8ba155bc4db9625124022e
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-05-20 20:03:05 +0100

    Fixed issues with java 8 and 11

--- debian/software/distro-cli/system/runtime/java8-openjdk-jre.sh
+++ debian/software/distro-cli/system/runtime/java8-openjdk-jre.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install software
-sudo apt-get install -y default-jre openjdk-8-jre;
+sudo apt-get install -y openjdk-8-jre;
 
 # Configure
 sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java;
--- ubuntu/software/distro-cli/development/java/java8-openjdk-jdk.sh
+++ ubuntu/software/distro-cli/development/java/java8-openjdk-jdk.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Install software
-sudo apt-get install -y default-jdk openjdk-8-jdk;
+sudo apt-get install -y openjdk-8-jdk;
 
 # Configure
 sudo update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac;
+sudo update-java-alternatives --set java-1.8.0-openjdk-amd64;
 
 # Verifications
 $(which javac) -version;
