420eff44a296a39ba7cf3e231b16c97c424b96a0
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-12-01 20:58:59 +0000

    Fixed dependency issue on distro-cli/drivers/grub2-efi.sh

--- ubuntu/software/distro-cli/drivers/grub2-efi.sh
+++ ubuntu/software/distro-cli/drivers/grub2-efi.sh
@@ -2,7 +2,7 @@
 
 # Install software
 sudo apt-get install -y grub2-common \
-                                   grub-efi-amd64 \
+                                   grub-efi-amd64-bin \
                                    grub-efi-amd64-signed \
                                    efibootmgr \
                                    efitools;
