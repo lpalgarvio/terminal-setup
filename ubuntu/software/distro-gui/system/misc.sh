516d3df4431976b38097573d0f2aa2478d851867
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-12-01 20:40:42 +0000

    Various fixes in distro-gui/system/misc.sh

--- ubuntu/software/distro-gui/system/misc.sh
+++ ubuntu/software/distro-gui/system/misc.sh
@@ -4,15 +4,15 @@
 sudo apt-get install -y ttf-mscorefonts-installer \
                                    konsole yakuake \
                                    dolphin dolphin-plugins \
-                                   software-properties-qt \
+                                   software-properties-kde \
                                    qapt-utils qapt-deb-installer \
                                    muon synaptic gtkorphan;
 
 # Verifications
 $(which konsole) --version;
-$(which yakuake) --version;
+#$(which yakuake) --version;
 $(which dolphin) --version;
-$(which software-properties-qt) --version;
+$(which software-properties-kde) --version;
 $(which qapt-deb-installer) --version;
 $(which muon) --version;
 $(which synaptic) -h;
