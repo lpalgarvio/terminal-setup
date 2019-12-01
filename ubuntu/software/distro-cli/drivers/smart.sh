ea3f82ad9399f00f84a5555608af3c2ee28cce4c
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-12-01 21:00:36 +0000

    Added idle3-tools and additional verifications to distro-cli/drivers/smart.sh

--- ubuntu/software/distro-cli/drivers/smart.sh
+++ ubuntu/software/distro-cli/drivers/smart.sh
@@ -3,9 +3,12 @@
 # Install software
 sudo apt-get install -y hdparm \
                                    sdparm \
+                                   idle3-tools \
                                    smartmontools;
 
 # Verifications
+$(which hdparm) -V;
 $(which sdparm) --version;
+$(which idle3ctl) -V;
 $(which smartctl) --version;
 
