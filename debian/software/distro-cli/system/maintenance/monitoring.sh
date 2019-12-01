49fa41eb9de927cf795cf9be61ed6b31310b7953
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-12-01 21:05:06 +0000

    Added nload and dstat to distro-cli/system/maintenance/monitoring.sh

--- debian/software/distro-cli/system/maintenance/monitoring.sh
+++ debian/software/distro-cli/system/maintenance/monitoring.sh
@@ -3,12 +3,15 @@
 # Install software
 sudo apt-get install -y htop \
                                    iotop \
-                                   iftop \
-                                   lsof;
+                                   iftop nload \
+                                   lsof \
+                                   dstat;
 
 # Verifications
 $(which htop) --version;
 $(which iotop) --version;
+$(which nload) -h;
 $(which iftop) -h;
 $(which lsof) -v;
+$(which dstat) -h;
 
