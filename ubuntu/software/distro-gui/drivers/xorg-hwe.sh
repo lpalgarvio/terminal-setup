284524295ac7032799583759dcf790629ed15d18
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-12-01 20:41:14 +0000

    Added xorg-hwe to distro-gui/drivers

---
+++ ubuntu/software/distro-gui/drivers/xorg-hwe.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install software
+sudo apt-get install -y xserver-xorg-hwe-18.04 \
+                                   xserver-xorg-core-hwe-18.04 \
+                                   xserver-xorg-input-all-hwe-18.04 \
+                                   xserver-xorg-video-all-hwe-18.04;
+
