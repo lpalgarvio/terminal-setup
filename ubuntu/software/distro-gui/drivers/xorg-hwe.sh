1192d06f25e7ad76e4b2a8ba0655c9f764e440ca
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2019-12-03 21:45:37 +0000

    Added xorg to distro-gui/drivers/xorg*.sh

--- ubuntu/software/distro-gui/drivers/xorg-hwe.sh
+++ ubuntu/software/distro-gui/drivers/xorg-hwe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install software
-sudo apt-get install -y xserver-xorg-hwe-18.04 \
+sudo apt-get install -y xorg xserver-xorg-hwe-18.04 \
                                    xserver-xorg-core-hwe-18.04 \
                                    xserver-xorg-input-all-hwe-18.04 \
                                    xserver-xorg-video-all-hwe-18.04;
--- ubuntu/software/distro-gui/drivers/xorg.sh
+++ ubuntu/software/distro-gui/drivers/xorg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install software
-sudo apt-get install -y xserver-xorg \
+sudo apt-get install -y xorg xserver-xorg \
                                    xserver-xorg-core \
                                    xserver-xorg-input-all \
                                    xserver-xorg-video-all;
